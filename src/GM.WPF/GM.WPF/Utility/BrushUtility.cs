9e38dcc5e896afd5e9484140450bb0a88e1bba93
Grega Mohorko <grega@mohorko.info>
2019-09-17 17:24:27 +0200

    Added BrushUtility.

---
+++ src/GM.WPF/GM.WPF/Utility/BrushUtility.cs
@@ -0,0 +1,58 @@
+﻿/*
+MIT License
+
+Copyright (c) 2019 Grega Mohorko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Project: GM.WPF
+Created: 2019-09-17
+Author: Grega Mohorko
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GM.WPF.Utility
+{
+	/// <summary>
+	/// Utilities for <see cref="Brush"/>.
+	/// </summary>
+	public static class BrushUtility
+	{
+		/// <summary>
+		/// Converts this <see cref="Brush"/> into a <see cref="System.Drawing.Color"/>.
+		/// <para>Currently, only implemented for <see cref="SolidColorBrush"/>.</para>
+		/// </summary>
+		/// <param name="brush">The <see cref="Brush"/> to convert to <see cref="System.Drawing.Color"/>.</param>
+		public static System.Drawing.Color ToDrawingColor(this Brush brush)
+		{
+			if(brush is SolidColorBrush solidBrush) {
+				Color color = solidBrush.Color;
+				return System.Drawing.Color.FromArgb(color.R, color.G, color.B);
+			} else {
+				throw new NotImplementedException($"Only implemented for {nameof(SolidColorBrush)}, sorry.");
+			}
+		}
+	}
+}
